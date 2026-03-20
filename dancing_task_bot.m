b0afcd3487cc6462efd49d4dd3b2eee3ead75aa3
westermannlab <stefan.westermann@uni-wh.de>
2026-03-20 13:21:01 +0100

    Add additional comments

--- dancing_task_bot.m
+++ dancing_task_bot.m
@@ -1,75 +1,74 @@
 function [Obs_d] = dancing_task_bot(rounds, visualize)
 
-% Lernparameter:
-alpha = 0.1;
-beta = 8;
-
-% Spielfeld und Start
-d = 10;
-dmax = 20;
-
-% Preference and Actions
-delta = 3;
-deltarange = 1;
-actions = [-1,0,1];
-nActions = length(actions);
-Q = zeros(1,nActions,dmax+1,nActions);
-
-lastChoices = [2,2];
-
-Obs_d = zeros(rounds,1);
-Obs_d(1,1) = d;
-
-action_other = 2;
-
-for round = 2:rounds
-
-    %action_other = 1;
-    %d = d+actions(action_other);
-
-    % action selection
-    expQ = exp(beta * Q(1,:,d+1,action_other));
-    choice = randsample(nActions,1,true,expQ);
-
-    % constrain range
-    if d >= dmax && choice == 3
-        choice = 2;
+    % Lernparameter:
+    alpha = 0.1; % Q learning
+    beta = 8; % action selection
+    
+    % Spielfeld und Start
+    d = 10; % distance to other (initial)
+    dmax = 20; % maximum distance
+    
+    % Preference and Actions
+    delta = 3; % preferred distance to other
+    deltarange = 1; % tolerance
+    actions = [-1, 0, 1]; % avoid, stay, approach (the other)
+    nActions = length(actions);
+    Q = zeros(1, nActions, dmax+1, nActions); % Initialize Q matrix (first dim: agent; here: only S)
+    
+    % Initial actions
+    lastChoices = [2,2];
+    action_other = find(actions == 0); % The action of the other is fixed to 
+    
+    % Observables
+    Obs_d = zeros(rounds,1);
+    Obs_d(1,1) = d;
+    
+    % Loop
+    for round = 2:rounds
+    
+        %action_other = 1;
+        %d = d+actions(action_other);
+    
+        % action selection
+        expQ = exp(beta * Q(1,:,d+1,action_other)); % Note: d+1 -> map distance to array index (d=0 -> index=1)
+        choice = randsample(nActions,1,true,expQ); % Choose action depending on Q values
+    
+        % constrain range
+        if d >= dmax && actions(choice) == +1 % If action would lead out of range...
+            choice = find(actions == 0); % ... change it to stay 
+        end
+        if d <= 0 && choice == 1 % If action would exceed maximal closeness...
+            choice = find(actions == 0); % .. change it to stay
+        end
+    
+        % Environment dynamics
+        dOld = d; 
+        d = d + actions(choice);
+    
+        % Reward
+        r = 10 * preference(0,d,delta,deltarange) - 5; % Why are those parameters (10, -5) not in the preference function?
+        %[d,r]
+    
+        % Q-learning
+        co = action_other;
+        Q(1, choice, dOld+1, co) = (1-alpha) * Q(1, choice, dOld+1, co) + alpha * r;
+    
+        lastChoices(1) = choice; % last choice of self
+    
+        % Model Observable
+        Obs_d(round,1) = d;
+    
+    
+    end % rounds
+    
+    %% Visualization
+    if visualize
+    
+        figure(10)
+        plot(Obs_d,'LineWidth',2)
+        ylim([0,dmax])
+        legend({'Distance'}, 'Location','best');
+        set(gca,'FontSize',14)
+    
     end
-    if d <= 0 && choice == 1
-        choice = 2;
-    end
-
-    % Environment dynamics
-    dOld = d;
-    d = d+actions(choice);
-
-    % Reward
-    r = 10*preference(0,d,delta,deltarange)-5;
-    %[d,r]
-
-    % Q-learning
-    co = action_other;
-    Q(1, choice, dOld+1,co) = (1-alpha)*Q(1, choice, dOld+1,co) + alpha * r;
-
-    lastChoices(1) = choice;
-
-    % Model Observable
-    Obs_d(round,1) = d;
-
-
-end % rounds
-
-%% Visualization
-if visualize
-
-    figure(10)
-    plot(Obs_d,'LineWidth',2)
-    ylim([0,dmax])
-    legend({'Distance'}, 'Location','best');
-    set(gca,'FontSize',14)
-
-end
-
-
-
 end
--- preference.m
+++ preference.m
@@ -1,9 +1,9 @@
-
+% Returns a Gaussian reward for how close the distance between two points is to a desired value.
 function reward = preference(xi,xj,delta,deltarange)
 % xi,xj: positions (scalar or vector)
 % delta: preferred distance
 % deltarange: tolerance (controls width)
-d = norm(xi - xj);
-sigma = max(deltarange, eps)/2;          % width
-reward = exp(-0.5*((d - delta)/sigma).^2);  % in [0,1], peaks at d=delta
+    d = norm(xi - xj);
+    sigma = max(deltarange, eps)/2;          % width
+    reward = exp(-0.5*((d - delta)/sigma).^2);  % in [0,1], peaks at d=delta
 end
