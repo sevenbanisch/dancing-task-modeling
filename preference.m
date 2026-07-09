c8da4e07d7cdd9c9c566ff399b178e9e45455ea5
Sven Banisch <43904247+sevenbanisch@users.noreply.github.com>
2026-07-09 17:32:03 +0200

    Update preference.m

--- preference.m
+++ preference.m
@@ -1,5 +1,5 @@
 % Returns a Gaussian reward for how close the distance between two points is to a desired value.
-function reward = preference(xi, xj, delta, deltarange,mode)
+function reward = preference(xi, xj, delta, deltarange,pref_mode)
     % xi,xj: positions (scalar or vector)
     % delta: preferred distance
     % deltarange: tolerance (controls width)
@@ -7,14 +7,14 @@ function reward = preference(xi, xj, delta, deltarange,mode)
     d = norm(xi - xj);
 
     % original preference function
-    if mode == "exp"        
+    if pref_mode == "exp"        
         sigma = max(deltarange, eps)/2;          % width
         reward = 2*exp(-0.5*((d - delta)/sigma).^2)-1;  % in [0,1], peaks at d=delta
-    elseif mode == "normdif"    % yet another alternative function, but non-linear
+    elseif pref_mode == "normdif"    % yet another alternative function, but non-linear
         difference = abs(d - delta);
         normed_difference = difference ./ deltarange;
         reward = (1 - normed_difference) ./ (1 + normed_difference);
-    elseif mode == "abs"    % alternative (linear) preference function       
+    elseif pref_mode == "abs"    % alternative (linear) preference function       
         difference = abs(d - delta);
         reward = 1 - difference ./ deltarange;
     else    
